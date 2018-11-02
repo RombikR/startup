ea35b7f4910ac5e210a3227e4d89154ad0b7db19
webchr <roman@webchr.ru>
2018-11-02 17:33:06 +0300

    beautiful html

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>StartUP</title>
-</head>  
-<body>  
+</head>
+<body>
     <header>+7(927)XXX XX XX</header>
     <nav><a href="index.php">Главная</a><a href="index2.php">Страница 2</a><a href="index3.php">Страница 3</a></nav>
     <section id="slider">
@@ -16,20 +16,21 @@
             <div class="item"><img src="/banner-003.jpg"></div>
         </div>
     </section>
-<section id="advantages">
-    <ul>
-        <li>1 Мы быстрее</li>
-        <li>2 Мы выше</li>
-        <li>3 Мы сильнее</li>
-    </ul>
-</section>
+    <section id="advantages">
+        <ul>
+            <li>1 Мы быстрее</li>
+            <li>2 Мы выше</li>
+            <li>3 Мы сильнее</li>
+        </ul>
+    </section>
     <section>
-<?php
-echo "Hello!!!";
-echo "Привет мир";
-echo "Страница 1";
-?>
+        <?php
+            echo "Hello!!!";
+            echo "Привет мир";
+            echo "Страница 1";
+        ?>
     </section>
     <footer>&copy;2018 все права защещины</footer>
-</body>  
+</body>
+
 </html>
