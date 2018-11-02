a4468cd734d91d6b0dad846d6eccaf9396e9fc2a
webchr <roman@webchr.ru>
2018-11-02 17:10:42 +0300

    Добавил страницу 3

---
+++ index3.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+    <title>StartUP</title>
+</head>  
+<body>  
+<header>+7(927)XXX XX XX</header>
+    <nav><a href="index2.php">Страница 2</a></nav>
+    <section>
+<?php
+echo "Hello!!!";
+echo "Привет мир";
+echo "Страница 3";
+?>
+    </section>
+    <footer>&copy;2018 все права защещины</footer>
+</body>  
+</html>
