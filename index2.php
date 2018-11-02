91c65d7c5f4521fef3fc8e90e5e60e5f3954eec7
webchr <roman@webchr.ru>
2018-11-02 16:42:06 +0300

    добавил разметку на страницу 2

--- index2.php
+++ index2.php
@@ -7,10 +7,15 @@
     <title>StartUP</title>
 </head>  
 <body>  
+<header>+7(927)XXX XX XX</header>
+    <nav><a href="index2.php">Страница 2</a></nav>
+    <section>
 <?php
 echo "Hello!!!";
 echo "Привет мир";
 echo "Страница 2";
 ?>
+    </section>
+    <footer>&copy;2018 все права защещины</footer>
 </body>  
 </html>
