70f64524cf862a01c4a7405ce760ac3063658571
RombikR <roman@webchr.ru>
2018-11-03 15:55:05 +0300

    Update index.php
    Вернул к исходному

--- index.php
+++ index.php
@@ -7,30 +7,5 @@
     <title>StartUP</title>
 </head>
 <body>
-    <header>+7(927)XXX XX XX</header>
-    <nav><a href="index.php">Главная</a><a href="index2.php">Страница 2</a><a href="index3.php">Страница 3</a></nav>
-    <section id="slider">
-        <div class="owl-carusel">
-            <div class="item"><img src="/banner-001.jpg"></div>
-            <div class="item"><img src="/banner-002.jpg"></div>
-            <div class="item"><img src="/banner-003.jpg"></div>
-        </div>
-    </section>
-    <section id="advantages">
-        <ul>
-            <li>1. Мы быстрее</li>
-            <li>2. Мы выше</li>
-            <li>3. Мы сильнее</li>
-        </ul>
-    </section>
-    <section>
-        <?php
-            echo "Hello!!!";
-            echo "Привет мир";
-            echo "Страница 1";
-        ?>
-    </section>
-    <footer>&copy;2018 все права защещины</footer>
 </body>
-
 </html>
