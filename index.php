904be4ab789e8b44c4442a9d013c3106b8f6c51d
webchr <roman@webchr.ru>
2018-11-03 16:33:02 +0300

    Добавил иконку вверх

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
             <div class="row">
                 <div class="col-4">&copy;2018 Все права защищены</div>
                 <div class="col-4">Developed by <a href="http://example.com">me</a></div>
-                <div class="col-4"><a href="#">To top</a></div>
+                <div class="col-4"><a href="#"><i class="fas fa-angle-up"></i> To top</a></div>
             </div>
         </div>
     </footer>
