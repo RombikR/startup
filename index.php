599ea5f69e6154c855d619d37bab939b7d1b3ff7
webchr <roman@webchr.ru>
2018-11-10 13:09:36 +0300

    Добавил восклицание

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <header>
         <div class="container">
             <div class="row">
-                <div class="col-12">HEADER</div>
+                <div class="col-12">HEADER!</div>
             </div>
         </div>
     </header>
